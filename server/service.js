b36395e13ea2a4091b2ec6c75037cc8c07e063de
y-akahori-ramen <y.akahori.ramen@gmail.com>
2022-10-19 00:25:27 +0900

    Remove unnecessary logs.

--- server/service.js
+++ server/service.js
@@ -7,24 +7,17 @@ export default class Service {
 
     constructor(storageDir) {
         this.#storageDir = storageDir;
-        console.log("constructor");
-        console.log(this.#storageDir);
     }
 
     handleHello(req, res) {
-        console.log('handle hello request');
         res.json({ message: "hello from server." });
     }
 
     uploadFile(req, res) {
-        console.log('handle uploadFile request');
-
-        console.log(this.#storageDir);
-
         const safeFilename = req.body.filename.replace(/\.\.\//g, "");
         const saveFilePath = path.resolve(this.#storageDir, safeFilename);
 
-        console.log(saveFilePath);
+        console.log(`save image: ${saveFilePath}`);
 
         writeFile(saveFilePath, req.body.base64data, { encoding: "base64" })
             .then(() => {
